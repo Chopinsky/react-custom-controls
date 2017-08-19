02f921a2f31c423d1249fa5f6973be6e010638ed
Jacob Zuo <chopinsky@live.com>
2017-08-19 18:23:30 -0500

    tracking test files as well

---
+++ test/selection-list.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { SelectionList } from '../src/index';
+
+describe("Selection List", () => {
+  let props;
+  let mountedComponent;
+
+  const selList = () => {
+    if (!mountedComponent) {
+      mountedComponent = mount(
+        <SelectionList {...props} />
+      );
+    }
+
+    return mountedComponent;
+  }
+
+  beforeEach(() => {
+    props = {
+      data: [{ id: "1", name: "test item 1" }],
+      selectionClassName: undefined,
+      controlClassName: undefined,
+      selectFirst: undefined
+    };
+    mountedLockScreen = undefined;
+  });
+
+  // All tests will go here
+  it("always renders a div", () => {
+    const divs = lockScreen().find("div");
+    const wrappingDiv = divs.first();
+
+    expect(divs.length).toBeGreaterThan(0);
+    expect(wrappingDiv.children()).toEqual(lockScreen().children());
+  });
+});
